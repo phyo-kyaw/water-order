2b831ad1ccb7173ff2ff0966189fd5a0c2a1a282
phyo-kyaw <phy0ky6w@gmail.com>
2021-03-20 10:37:51 +1100

    1st commit

---
+++ src/main/java/com/rubicon/waterorder/WaterOrderApplication.java
@@ -0,0 +1,13 @@
+package com.rubicon.waterorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterOrderApplication.class, args);
+    }
+
+}
